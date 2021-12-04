63a12fdf82296b29e530808d71be22e099a4e14b
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2021-12-03 21:07:58 -0300

    Ajustes gerais

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("snap-modal").then((cache) =>
-      cache.addAll([
-        "/snap-modal",
-        // "/snap-modal/index.html",
-        // "/snap-modal/index.js",
-        // "/snap-modal/index.css",
-      ])
-    )
-  );
-});
+// self.addEventListener("install", (e) => {
+//   e.waitUntil(
+//     caches
+//       .open("snap-modal")
+//       .then((cache) =>
+//         cache.addAll([
+//           "/snap-modal",
+//           "/snap-modal/index.html",
+//           "/snap-modal/index.js",
+//           "/snap-modal/index.css",
+//         ])
+//       )
+//   );
+// });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
